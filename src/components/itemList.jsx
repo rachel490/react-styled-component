b126733cd8635464685d9e21ac7d5f4c0de9b95d
rachel490 <72786354+rachel490@users.noreply.github.com>
2021-10-05 00:30:04 +0900

    sending svg file as a variable to image src

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import Item from './item';
+import growTogetherImage from '../static/images/illustration-grow-together.svg';
+import flowingConversationImage from '../static/images/illustration-flowing-conversation.svg';
+import yourUsersImage from '../static/images/illustration-your-users.svg';
+
 
 const data = [
     {
         "title":"Grow Together",
         "desc":"Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.",
-        "img":"../static/images/illustration-grow-together.svg"
+        "img":`${growTogetherImage}`
     },
     {
         "title":"Flowing Conversations",
         "desc":"You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
-        "img":"../static/images/illustration-flowing-conversation.svg"
+        "img":`${flowingConversationImage}`
     },
     {
         "title":"Your Users",
         "desc":"It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.",
-        "img":"../static/images/illustration-your-users.svg"
+        "img":`${yourUsersImage}`
     }
 ];
 
